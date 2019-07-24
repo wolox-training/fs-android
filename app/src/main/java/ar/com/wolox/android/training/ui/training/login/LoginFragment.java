dc0f125d664631e9f82bc206e941404491af6571
Federico Sanucci <federico.sanucci@wolox.com.ar>
2019-07-24 09:31:13 -0300

    checkstyle fixed

--- app/src/main/java/ar/com/wolox/android/training/ui/training/login/LoginFragment.java
+++ app/src/main/java/ar/com/wolox/android/training/ui/training/login/LoginFragment.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
